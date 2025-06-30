6254686be24d4fdff395b50e464c5a30ced30463
dudamoraes1 <166837187+dudamoraes1@users.noreply.github.com>
2025-06-30 07:43:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual foi a capital do Império Romano?',
+    'Roma'
+)
+
+criaCartao(
+    'geral',
+    'Quantos meses tem um ano?',
+    '12'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o nome do maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca'
+)
